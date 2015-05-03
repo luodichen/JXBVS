9dcc61801afdbfe7dc279c106e4b1a74fa7ad4c5
luodichen <me@luodichen.com>
2015-05-03 17:39:26 +0800

    modify sample checklist

--- project/client/checklists/SampleCheckList.py
+++ project/client/checklists/SampleCheckList.py
@@ -1,11 +1,11 @@
-import BaseCheckList
+from BaseCheckList import BaseCheckList
 
 class SampleCheckList(BaseCheckList):
     def __init__(self):
-        pass
+        BaseCheckList.__init__(self)
         
     def get_id(self):
         return 1 # sample checklist id
     
     def check(self):
-        print id
+        print self.id
